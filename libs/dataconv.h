6834ef224f80596ca404e45c0d54649e3261b64f
Max Gilmour <mgilmour@maxgmr.ca>
2025-01-30 12:22:22 -0800

    add millivolt conversion

--- libs/dataconv.h
+++ libs/dataconv.h
@@ -3,5 +3,7 @@
 
 // Get the 10-bit analogue to digital converter reading as volts.
 #define ADC_TO_V(adc) ((5. / 1023.) * adc)
+// Get the 10-bit analogue to digital converter reading as millivolts.
+#define ADC_TO_MILLIV(adc) ((5000. / 1023.) * adc)
 
 #endif // DATACONV_H
